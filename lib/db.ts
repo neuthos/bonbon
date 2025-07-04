c18ed1f8e343cd39ac5dbe604041c65877ccf95b
Galang Rafly Ardian <72038491+neuthos@users.noreply.github.com>
2025-07-04 22:24:38 +0700

    fix: add turso

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,26 @@
 import {PrismaClient} from "@prisma/client";
+import {PrismaLibSQL} from "@prisma/adapter-libsql";
+import {createClient} from "@libsql/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db =
-  globalThis.prisma ||
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+function createPrismaClient() {
+  if (process.env.TURSO_DATABASE_URL && process.env.TURSO_AUTH_TOKEN) {
+    const libsql = createClient({
+      url: process.env.TURSO_DATABASE_URL,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+    });
+
+    const adapter = new PrismaLibSQL(libsql);
+    return new PrismaClient({adapter});
+  } else {
+    return new PrismaClient();
+  }
+}
+
+export const db = globalThis.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
